83f97fa0422a11375f6ec0cdcf6ab89467642b7c
anupamsaini <anupamsaini@gmail.com>
2014-06-25 16:20:30 +0530

    Updated Gson reading guice modules

--- src/com/scrappy/json/GsonReader.java
+++ src/com/scrappy/json/GsonReader.java
@@ -1,19 +1,11 @@
 package com.scrappy.json;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
-import com.scrappy.json.desrializer.ElementJsonDeserializer;
-import com.scrappy.json.desrializer.RelativeDeserializer;
-import com.scrappy.json.desrializer.ScrapeJsonDeserializer;
-import com.scrappy.json.desrializer.StepJsonDeserializer;
-import com.scrappy.protogen.Scrape.Element;
-import com.scrappy.protogen.Scrape.Relative;
 import com.scrappy.protogen.Scrape.Scraper;
-import com.scrappy.protogen.Scrape.Step;
 import com.scrappy.sites.SitesModule;
 
 import java.io.IOException;
@@ -25,18 +17,11 @@ import java.io.Reader;
  */
 public class GsonReader {
 
-  private final GsonBuilder gsonBuilder;
+  private final Gson gson;
 
   @Inject
-  public GsonReader(ElementJsonDeserializer elementJsonDeserializer,
-      RelativeDeserializer relativeDeserializer, ScrapeJsonDeserializer scarpeJsonDeserializer,
-      StepJsonDeserializer stepJsonDeserializer, GsonBuilder gsonBuilder) {
-    this.gsonBuilder = gsonBuilder;
-    // Configure GSON
-    gsonBuilder.registerTypeAdapter(Element.class, elementJsonDeserializer);
-    gsonBuilder.registerTypeAdapter(Relative.class, relativeDeserializer);
-    gsonBuilder.registerTypeAdapter(Scraper.class, scarpeJsonDeserializer);
-    gsonBuilder.registerTypeAdapter(Step.class, stepJsonDeserializer);
+  public GsonReader(Gson gson) {
+    this.gson = gson;
   }
 
   /**
@@ -46,8 +31,7 @@ public class GsonReader {
    * @return the {@link Scraper} representation of json data
    */
   public Scraper jsonStringToObject(Reader data) {
-    final Gson gson = gsonBuilder.create();
-    final Scraper scrape = gson.fromJson(data, Scraper.class);
+    Scraper scrape = gson.fromJson(data, Scraper.class);
     System.out.println(scrape);
     return scrape;
   }
--- src/com/scrappy/sites/SitesModule.java
+++ src/com/scrappy/sites/SitesModule.java
@@ -1,12 +1,22 @@
 package com.scrappy.sites;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 
-import com.scrappy.json.GsonReader;
 import com.scrappy.json.desrializer.ElementJsonDeserializer;
 import com.scrappy.json.desrializer.RelativeDeserializer;
 import com.scrappy.json.desrializer.ScrapeJsonDeserializer;
 import com.scrappy.json.desrializer.StepJsonDeserializer;
+import com.scrappy.protogen.Scrape.Element;
+import com.scrappy.protogen.Scrape.Relative;
+import com.scrappy.protogen.Scrape.Scraper;
+import com.scrappy.protogen.Scrape.Step;
 
 /**
  * Configuration for sites module.
@@ -17,14 +27,23 @@ public class SitesModule extends AbstractModule {
   protected void configure() {
     bind(ScrapeConfig.class).to(ScrapeConfigImpl.class);
 
-    //TODO(anupam): Bind these to interface and use annotatedBy.
-    bind(ElementJsonDeserializer.class);
-    bind(RelativeDeserializer.class);
-    bind(ScrapeJsonDeserializer.class);
-    bind(StepJsonDeserializer.class);
+    bind(new TypeLiteral<JsonDeserializer<Element>>() {}).to(ElementJsonDeserializer.class);
+    bind(new TypeLiteral<JsonDeserializer<Relative>>() {}).to(RelativeDeserializer.class);
+    bind(new TypeLiteral<JsonDeserializer<Scraper>>() {}).to(ScrapeJsonDeserializer.class);
+    bind(new TypeLiteral<JsonDeserializer<Step>>() {}).to(StepJsonDeserializer.class);
+  }
 
-    // Inject validators.
-//    bind(Validatable.class).to(ElementValidtor.class);
-    bind(GsonReader.class);
+  @Provides
+  private Gson registerDesearilizerTypeAdaptors(Injector injector) {
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.registerTypeAdapter(Element.class,
+        injector.getInstance(Key.get(new TypeLiteral<JsonDeserializer<Element>>() {})));
+    gsonBuilder.registerTypeAdapter(Relative.class,
+        injector.getInstance(Key.get(new TypeLiteral<JsonDeserializer<Relative>>() {})));
+    gsonBuilder.registerTypeAdapter(Step.class,
+        injector.getInstance(Key.get(new TypeLiteral<JsonDeserializer<Step>>() {})));
+    gsonBuilder.registerTypeAdapter(Scraper.class,
+        injector.getInstance(Key.get(new TypeLiteral<JsonDeserializer<Scraper>>() {})));
+    return gsonBuilder.create();
   }
 }
